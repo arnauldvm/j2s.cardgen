c391a7697bb856be8f0e1a700832fdfb740cfba5
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-06-28 18:49:36 +0200

    :bug: Preserver size ratio for picture

--- assets/card/index.js
+++ assets/card/index.js
@@ -35,7 +35,7 @@ const drawGrid = function(/* CanvasRenderingContext2D */ ctx, /* String of [0-9]
 
 const context = require('../context');
 
-const drawImage = function(/* CanvasRenderingContext2D */ ctx, /* string */ imgUrl, w, h, /* boolean */ hasBorder) {
+const drawImage = function(/* CanvasRenderingContext2D */ ctx, /* string */ imgUrl, width, height, /* boolean */ hasBorder) {
   // (saving the context, since it will likely have changed by the time the image is actually drawn
   //  (because of async loading))
   const curContext = new context.SavedContext(ctx);
@@ -43,8 +43,21 @@ const drawImage = function(/* CanvasRenderingContext2D */ ctx, /* string */ imgU
   img.addEventListener('load', function() {
     const saveContext = new context.SavedContext(ctx);
     curContext.restoreIn(ctx);
-    ctx.drawImage(img, 0, 0, w, h); // TODO: should preserve size ratio
-    if (hasBorder) ctx.strokeRect(0, 0, w, h);
+    const imgRatio = img.height/img.width;
+    const targetRatio = height/width;
+    let x = 0;
+    let y = 0;
+    let w = width;
+    let h = height;
+    if (imgRatio>targetRatio) {
+      w = height/imgRatio;
+      x = (width-w)/2;
+    } else {
+      h = width*imgRatio;
+      y = (height-h)/2;
+    }
+    ctx.drawImage(img, x, y, w, h);
+    if (hasBorder) ctx.strokeRect(0, 0, width, height);
     saveContext.restoreIn(ctx);
   }, false);
   img.src = cardDescription.picture;
