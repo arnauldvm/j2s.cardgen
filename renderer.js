acd7aa899cb8bdd10e587d91f677b3e7a5b24b8a
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-07-01 18:57:32 +0200

    :sparkles: Now possible to generate several copies of the card

--- renderer.js
+++ renderer.js
@@ -17,10 +17,16 @@ const cardDescription = require("./data/card.json");
 
 const card = require('./assets/card');
 
-const canvas = document.getElementById('card');
-if (canvas.getContext) {
-  const ctx = canvas.getContext('2d');
-  card.draw(ctx, cardDescription);
-} else {
-  // canvas unsupported
+const div = document.getElementById('cards');
+for (let idx=0; idx<cardDescription.count; idx++) {
+  const canvas = document.createElement('canvas');
+  canvas.classList.add('card_frame');
+  canvas.width = 300;
+  canvas.height = 200;
+  if (!canvas.getContext) {
+    alert("Canvas unsupported");
+    break;
+  }
+  card.draw(canvas.getContext('2d'), cardDescription);
+  div.appendChild(canvas);
 }
