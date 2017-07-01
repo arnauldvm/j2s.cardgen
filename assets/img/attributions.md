b4205898741855429a2368f1e1d486c829c75eab
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-07-01 20:28:48 +0200

    :sparkles: Now possible to render several different cards

--- assets/img/attributions.md
+++ assets/img/attributions.md
@@ -1,1 +1,3 @@
 * https://pixabay.com/p-31607/?no_redirect
+* https://s-media-cache-ak0.pinimg.com/originals/9e/bb/ac/9ebbaceb83bf6d4751cd657f11384e16.jpg
+* http://orig10.deviantart.net/5dea/f/2012/110/3/1/profile_picture_by_awesome_orc-d4wzzme.jpg
--- renderer.js
+++ renderer.js
@@ -16,20 +16,22 @@ ipc.on('failed-pdf', function (event, error) {
 });
 
 
-const cardDescription = require("./data/card.json");
+const cardDescriptions = require("./data/cards.json");
 
 const card = require('./assets/card');
 
 const div = document.getElementById('cards');
-for (let idx=0; idx<cardDescription.count; idx++) {
-  const canvas = document.createElement('canvas');
-  canvas.classList.add('card_frame');
-  canvas.width = 300;
-  canvas.height = 200;
-  if (!canvas.getContext) {
-    alert("Canvas unsupported");
-    break;
+cardDescriptions.forEach(function(cardDescription) {
+  for (let idx=0; idx<cardDescription.count; idx++) {
+    const canvas = document.createElement('canvas');
+    canvas.classList.add('card_frame');
+    canvas.width = 300;
+    canvas.height = 200;
+    if (!canvas.getContext) {
+      alert("Canvas unsupported");
+      break;
+    }
+    card.draw(canvas.getContext('2d'), cardDescription);
+    div.appendChild(canvas);
   }
-  card.draw(canvas.getContext('2d'), cardDescription);
-  div.appendChild(canvas);
-}
+});
