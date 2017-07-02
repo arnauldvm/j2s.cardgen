9113223e458f4aa2ee5ac9bb16032d7a799acb31
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-07-02 12:51:21 +0200

    :lipstick: Set the size dynamically

--- renderer.js
+++ renderer.js
@@ -25,6 +25,8 @@ cardDescriptions.forEach(function(cardDescription) {
   for (let idx=0; idx<cardDescription.count; idx++) {
     const canvas = document.createElement('canvas');
     canvas.classList.add('card_frame');
+    canvas.style.width = "6cm";
+    canvas.style.height = "4cm";
     canvas.width = 300;
     canvas.height = 200;
     if (!canvas.getContext) {
