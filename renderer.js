5c9f8bb379bad2503607dcfc01d55d3c77088858
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-07-03 21:39:05 +0200

    :lipstick: Trigger display of cards by a button

--- renderer.js
+++ renderer.js
@@ -18,23 +18,27 @@ ipc.on('failed-pdf', function (event, error) {
 });
 
 
-const cardDescriptions = require("./data/cards.json");
-
-const card = require('./assets/card');
-
-const div = document.getElementById('cards');
-const params = cardDescriptions.parameters;
-cardDescriptions.cards.forEach(function(cardDescription) {
-  for (let idx=0; idx<cardDescription.count; idx++) {
-    const canvas = document.createElement('canvas');
-    canvas.classList.add('card_frame');
-    Object.assign(canvas.style, params.size);
-    Object.assign(canvas, params.resolution);
-    if (!canvas.getContext) {
-      alert("Canvas unsupported");
-      break;
+const displayCards = function() {
+  const cardDescriptions = require("./data/cards.json");
+  const card = require('./assets/card');
+  const div = document.getElementById('cards');
+  const params = cardDescriptions.parameters;
+  cardDescriptions.cards.forEach(function(cardDescription) {
+    for (let idx=0; idx<cardDescription.count; idx++) {
+      const canvas = document.createElement('canvas');
+      canvas.classList.add('card_frame');
+      Object.assign(canvas.style, params.size);
+      Object.assign(canvas, params.resolution);
+      if (!canvas.getContext) {
+        alert("Canvas unsupported");
+        break;
+      }
+      card.draw(canvas.getContext('2d'), params, cardDescription);
+      div.appendChild(canvas);
     }
-    card.draw(canvas.getContext('2d'), params, cardDescription);
-    div.appendChild(canvas);
-  }
-});
+  });
+  addMessage("Cards displayed");
+};
+
+document.getElementById('cardsBtn').addEventListener('click', displayCards);
+
